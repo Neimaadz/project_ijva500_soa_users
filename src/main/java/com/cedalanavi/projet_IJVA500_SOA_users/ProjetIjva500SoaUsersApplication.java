106bf6e27af5f1685d009f206c7b6af8ef4ea3a3
Neimaad <damienlaoussing@hotmail.com>
2022-10-17 15:19:56 +0200

    first commit

---
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_users/ProjetIjva500SoaUsersApplication.java
@@ -0,0 +1,13 @@
+package com.cedalanavi.projet_IJVA500_SOA_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIjva500SoaUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIjva500SoaUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedalanavi/projet_IJVA500_SOA_users/ProjetIjva500SoaUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedalanavi.projet_IJVA500_SOA_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIjva500SoaUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
